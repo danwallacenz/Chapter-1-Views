8ef3117b2978fbb95f7ee34fbfcedaa742e18a42
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-04-22 14:05:27 +1200

    Refactor view logging into view:Name:

--- Chapter 1 - Views/ViewController.m
+++ Chapter 1 - Views/ViewController.m
@@ -23,36 +23,28 @@
     UIView* v = [[UIView alloc] initWithFrame: CGRectMake(100,100,50,50)];
     v.backgroundColor = [UIColor redColor];
     [mainView addSubview: v];
-    
-    NSLog(@"mainView frame origin.x = %f", mainView.frame.origin.x);
-    NSLog(@"mainView frame origin.y = %f", mainView.frame.origin.y);
-    NSLog(@"mainView frame size.height = %f", mainView.frame.size.height);
-    NSLog(@"mainView frame size.width = %f", mainView.frame.size.width);
-    
-    NSLog(@"mainView bounds origin.x = %f", mainView.bounds.origin.x);
-    NSLog(@"mainView bounds origin.y = %f", mainView.bounds.origin.y);
-    NSLog(@"mainView bounds size.height = %f", mainView.bounds.size.height);
-    NSLog(@"mainView bounds size.width = %f", mainView.bounds.size.width);
-    
-    NSLog(@"mainView center.x = %f", mainView.center.x);
-    NSLog(@"mainView center.y = %f", mainView.center.y);
 
+    [self logView: mainView name: @"mainView"];
     
-    NSLog(@"v frame origin.x = %f", v.frame.origin.x);
-    NSLog(@"v frame origin.y = %f", v.frame.origin.y);
-    NSLog(@"v frame size.height = %f", v.frame.size.height);
-    NSLog(@"v frame size.width = %f", v.frame.size.width);
-    
-    NSLog(@"v bounds origin.x = %f", v.bounds.origin.x);
-    NSLog(@"v bounds origin.y = %f", v.bounds.origin.y);
-    NSLog(@"v bounds size.height = %f", v.bounds.size.height);
-    NSLog(@"v bounds size.width = %f", v.bounds.size.width);
-    
-    NSLog(@"v center.x = %f", v.center.x);
-    NSLog(@"v center.y = %f", v.center.y);
-    
-    
+    [self logView: v name: @"v"];
 }
+
+- (void)logView: (UIView*)v name: (NSString*) name
+{
+    NSLog(@"%@ frame origin.x = %f", name, v.frame.origin.x);
+    NSLog(@"%@ frame origin.y = %f", name, v.frame.origin.y);
+    NSLog(@"%@ frame size.height = %f", name, v.frame.size.height);
+    NSLog(@"%@ frame size.width = %f", name, v.frame.size.width);
+    
+    NSLog(@"%@ bounds origin.x = %f", name, v.bounds.origin.x);
+    NSLog(@"%@ bounds origin.y = %f", name, v.bounds.origin.y);
+    NSLog(@"%@ bounds size.height = %f", name, v.bounds.size.height);
+    NSLog(@"%@ bounds size.width = %f", name, v.bounds.size.width);
+    
+    NSLog(@"%@ center.x = %f", name, v.center.x);
+    NSLog(@"%@ center.y = %f", name, v.center.y);
+}
+
 
 - (void)didReceiveMemoryWarning
 {
