9107430ce889852eefe2c3bc229ccf4ffa7f27b2
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-04-22 13:33:07 +1200

    Initial commit

---
+++ Chapter 1 - Views/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Chapter 1 - Views
+//
+//  Created by Daniel Wallace [DATACOM] on 22/04/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
